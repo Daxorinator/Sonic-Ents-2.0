138325bee42766a0ef3da1d86a1a8d600cf4d576
Daxorinator <daxorinator@gmail.com>
2018-03-19 14:10:12 +0000

    Changed Index to PHP and merged form handler

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
 								</a>
 							</li><br><br>
 							<div class="form-area">  
-									<form role="form" name="contactform" method="POST" action="email.php">
+									<form role="form" name="contactform" method="POST" action="index.php">
 										<br style="clear:both">
 										<h3 style="margin-bottom: 25px; text-align: center;">Leave a message</h3>
 										<div class="form-group">
@@ -154,9 +154,88 @@
 										<div class="form-group">
 											<textarea class="form-control" type="textarea" name="message" id="message" placeholder="Message" maxlength="5000" rows="12" required></textarea>                   
 										</div>
-										<input type="submit" id="submit" value="submit" name="submit" class="btn pull-right">Submit Form</input>
+										<input type="submit" id="submit" value="submit" name="submit" class="btn pull-right"></input>
 									</form>
 							</div>
+							<?php
+								if(isset($_POST['email'])) {
+								
+										// EDIT THE 2 LINES BELOW AS REQUIRED
+										$email_to = "daxorinator@gmail.com";
+										$email_subject = "Sonic-Ents:" $_POST['subject'];
+								
+										function died($error) {
+												// your error code can go here
+												echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+												echo "These errors appear below.<br /><br />";
+												echo $error."<br /><br />";
+												echo "Please go back and fix these errors.<br /><br />";
+												die();
+										}
+								
+								
+										// validation expected data exists
+										if(!isset($_POST['name']) ||
+												!isset($_POST['email']) ||
+												!isset($_POST['subject']) ||
+												!isset($_POST['message'])) {
+												died('We are sorry, but there appears to be a problem with the form you submitted.');       
+										}
+								
+										
+								
+										$name = $_POST['name']; // required
+										$email = $_POST['email']; // required
+										$mobile = $_POST['mobile']; // required
+										$subject = $_POST['subject']; // not required
+										$message = $_POST['message']; // required
+								
+										$error_message = "";
+										$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+								
+									if(!preg_match($email_exp,$email)) {
+										$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+									}
+								
+										$string_exp = "/^[A-Za-z .'-]+$/";
+								
+									if(!preg_match($string_exp,$name)) {
+										$error_message .= 'The name you entered does not appear to be valid.<br />';
+									}
+								
+									if(!preg_match($string_exp,$subject)) {
+										$error_message .= 'The subject you entered does not appear to be valid.<br />';
+									}
+								
+									if(strlen($message) < 5) {
+										$error_message .= 'The message you entered do not appear to be valid.<br />';
+									}
+								
+									if(strlen($error_message) > 0) {
+										died($error_message);
+									}
+								
+										$email_message = "Form details below.\n\n";
+								
+										
+										function clean_string($string) {
+											$bad = array("content-type","bcc:","to:","cc:","href");
+											return str_replace($bad,"",$string);
+										}
+								
+										
+								
+										$email_message .= "Name: ".clean_string($name)."\n";
+										$email_message .= "Email: ".clean_string($email)."\n";
+										$email_message .= "Phone: ".clean_string($mobile)."\n";
+										$email_message .= "Subject: ".clean_string($subject)."\n";
+										$email_message .= "Message: ".clean_string($message)."\n";
+								
+								// create email headers
+								$headers = 'From: '.$email_from."\r\n".
+								'Reply-To: '.$email_from."\r\n" .
+								'X-Mailer: PHP/' . phpversion();
+								@mail($email_to, $email_subject, $email_message, $headers);?>
 						</ul>
 					</div>
 				</div>
